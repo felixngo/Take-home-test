4a05f988bc38d4e947a2e461dd03587e2d8dc226
felix <felix7051@gmail.com>
2024-09-24 16:07:15 +0100

    [Refactor] Move updateBenefitValue logic into the drug model

--- Models/Drug.js
+++ Models/Drug.js
@@ -4,4 +4,79 @@ export class Drug {
         this.expiresIn = expiresIn;
         this.benefit = benefit;
     }
+
+    updateExpiresIn() {
+        if (this.name != "Magic Pill")
+        {
+            this.expiresIn--;
+        }
+    }
+
+    updateBenefit() {
+        if (this.name == "Magic Pill")
+        {
+            return;
+        }
+        if (this.name == "Herbal Tea")
+        {
+            this.increaseBenefit();
+        }
+        else if (this.name == "Fervex") {
+            if (this.expiresIn > 0) {
+                this.increaseBenefit();
+            } else {
+                this.benefit = 0;
+            }
+        }
+        else
+        {
+            this.decreaseBenefit();
+        }
+    }
+
+    decreaseBenefit() {
+        if (this.benefit == 0)
+        {
+            return;
+        }
+        if (this.expiresIn > 0)
+        {
+            this.benefit -= 1;
+        }
+        else
+        {
+            this.benefit -= 2;
+        }
+    }
+
+    increaseBenefit() {
+        if (this.benefit >= 50)
+        {
+            return;
+        }
+        if (this.name == "Herbal Tea")
+        {
+            this.benefit += this.expiresIn > 0 ? 1 : 2;
+        }
+        if (this.name == "Fervex")
+        {
+            if (this.expiresIn <= 5)
+            {
+                this.benefit += 3;
+            }
+            else if (this.expiresIn <= 10)
+            {
+                this.benefit += 2;
+            }
+            else
+            {
+                this.benefit += 1;
+            }
+        }
+
+        if (this.benefit > 50)
+        {
+            this.benefit = 50;
+        }
+    }
 }
